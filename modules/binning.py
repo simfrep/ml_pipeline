5b856362185ea18e86daf7f64495533eb468828a
Simon Preis <simfrep@gmail.com>
2020-10-27 22:43:10 +0100

    Convert into classes

--- modules/binning.py
+++ modules/binning.py
@@ -7,80 +7,88 @@ import logging
 
 #logging.basicConfig(level=logging.DEBUG)
 
-def iv_varsel(config = None, **kwargs):
-
-    if config is None:
-        logging.info("No Config file provided")
-    else:
-        logging.info("Config file provided")
-        datafile = config['dpath']+config['data']
-        data = pd.read_parquet(datafile)
-        bads = config['bads']
-        tgt = config['target']
-        dpath = config['dpath']
-        dvarsel = config['dvarsel']
-
-    collst =list(data.columns)
-    #logging.debug(collst)
-    feat = sorted(list(set(collst)-set(bads)-set(tgt)))
-    #logging.debug(feat)
-    data = data[feat+[tgt]]
-    logging.debug(data[tgt].value_counts())
-    data = sc.var_filter(data, y= tgt)
+class Binning():
     
-    # Ensure Output path exists. If not create folder
-    Path(dpath).mkdir(parents=True, exist_ok=True)
+    def __init__(
+        self,
+        config
+    ):
+        self.config = config
 
-    outfile = dpath+dvarsel
-    if os.path.isfile(dpath+dvarsel):
-        logging.info('File already exists. Add timestamp suffix')
-        _suffix = pd.Timestamp('now').strftime('%Y%m%d_%H:%m:%S')
-        outfile = dpath+dvarsel+_suffix
-    data.to_parquet(outfile)
+    def iv_varsel(self, **kwargs):
 
-def woe_bins(config = None, **kwargs):
-
-    if config is None:
-        logging.info("No Config file provided")
-    else:
-        logging.info("Config file provided")
-
-        datafile = config['dpath']+config['dvarsel']
-        if os.path.isfile(datafile):
-            logging.info(f"Preprocessed Datafile found {datafile}")
-            
+        if self.config is None:
+            logging.info("No Config file provided")
         else:
-            datafile = config['dpath']+config['data']
-            logging.info(f"No Preprocessed Datafile found. Default to {datafile}")
+            logging.info("Config file provided")
+            datafile = self.config['dpath']+self.config['data']
+            data = pd.read_parquet(datafile)
+            bads = self.config['bads']
+            tgt = self.config['target']
+            dpath = self.config['dpath']
+            dvarsel = self.config['dvarsel']
 
-        data = pd.read_parquet(datafile)
-        bads = config['bads']
-        tgt = config['target']
-        dpath = config['dpath']
-        dbins = config['dbins']
-        ppath = config['ppath']
-    
-    collst =list(data.columns)
-    feat = sorted(list(set(collst)-set(bads)))
+        collst =list(data.columns)
+        #logging.debug(collst)
+        feat = sorted(list(set(collst)-set(bads)-set(tgt)))
+        #logging.debug(feat)
+        data = data[feat+[tgt]]
+        logging.debug(data[tgt].value_counts())
+        data = sc.var_filter(data, y= tgt)
+        
+        # Ensure Output path exists. If not create folder
+        Path(dpath).mkdir(parents=True, exist_ok=True)
 
-    data = data[feat+[tgt]]
-    bins = sc.woebin(data, y=tgt)
-    joblib.dump(bins, dpath+dbins)
+        outfile = dpath+dvarsel
+        if os.path.isfile(dpath+dvarsel):
+            logging.info('File already exists. Add timestamp suffix')
+            _suffix = pd.Timestamp('now').strftime('%Y%m%d_%H:%m:%S')
+            outfile = dpath+dvarsel+_suffix
+        data.to_parquet(outfile)
 
-    # Create and save plots
-    plotlist = sc.woebin_plot(bins)
+    def woe_bins(self, **kwargs):
 
-    # Create df to rank variables by IV
-    iv_rank = pd.DataFrame()
-    for key in bins.keys():
-        iv_rank.loc[key,'iv']=bins[key]['bin_iv'].sum()
-    iv_rank['iv_rank'] = iv_rank['iv'].rank(ascending=False)
+        if self.config is None:
+            logging.info("No Config file provided")
+        else:
+            logging.info("Config file provided")
 
-    # Ensure Output path exists. If not create folder
-    Path(ppath).mkdir(parents=True, exist_ok=True)
-    # Save Plots with rank in filename
-    for i,x in iv_rank.iterrows():
-        rk = int(x['iv_rank'])
-        #print(f'{rk:03}')
-        plotlist[i].savefig(ppath+f'{rk:03}'+'_'+(i)+'.png')
+            datafile = self.config['dpath']+self.config['dvarsel']
+            if os.path.isfile(datafile):
+                logging.info(f"Preprocessed Datafile found {datafile}")
+                
+            else:
+                datafile = self.config['dpath']+self.config['data']
+                logging.info(f"No Preprocessed Datafile found. Default to {datafile}")
+
+            data = pd.read_parquet(datafile)
+            bads = self.config['bads']
+            tgt = self.config['target']
+            dpath = self.config['dpath']
+            dbins = self.config['dbins']
+            ppath = self.config['ppath']
+        
+        collst =list(data.columns)
+        feat = sorted(list(set(collst)-set(bads)))
+
+        data = data[feat+[tgt]]
+        bins = sc.woebin(data, y=tgt)
+        joblib.dump(bins, dpath+dbins)
+
+        # Create and save plots
+        plotlist = sc.woebin_plot(bins)
+
+        # Create df to rank variables by IV
+        iv_rank = pd.DataFrame()
+        for key in bins.keys():
+            iv_rank.loc[key,'iv']=bins[key]['bin_iv'].sum()
+        iv_rank['iv_rank'] = iv_rank['iv'].rank(ascending=False)
+
+        # Ensure Output path exists. If not create folder
+        Path(ppath).mkdir(parents=True, exist_ok=True)
+        # Save Plots with rank in filename
+        for i,x in iv_rank.iterrows():
+            rk = int(x['iv_rank'])
+            #print(f'{rk:03}')
+            plotlist[i].savefig(ppath+f'{rk:03}'+'_'+(i)+'.png')
 
--- modules/fit.py
+++ modules/fit.py
@@ -10,260 +10,215 @@ from sklearn.model_selection import ParameterGrid
 from sklearn.pipeline import Pipeline
 from pathlib import Path
 
+
 import random
 import importlib
 
+import binning
+from util import func_from_string, get_feat, get_data
 
-def func_from_string(key):
-    _mod = key.rsplit('.',1)[0]
-    _fun = key.rsplit('.',1)[1]
-    mod = importlib.import_module(_mod)
-    return getattr(mod, _fun)
+class MLPipeline(binning.Binning):
 
-def model_config(config):
-    m_dict=config['models']
-    if 'preprocessing' in config.keys():
-        preprocessing = config['preprocessing']
+    def __init__(
+        self,
+        config
+    ):
+        self.config = config
 
-    models = {}
 
-    if config['use_binning'] in [False, 'Both']:
-        for trans in preprocessing.keys():
-            trans_func = func_from_string(trans)
+    def model_config(self):
+        m_dict=self.config['models']
+        if 'preprocessing' in self.config.keys():
+            preprocessing = self.config['preprocessing']
+
+        models = {}
+
+        if self.config['use_binning'] in [False, 'Both']:
+            for trans in preprocessing.keys():
+                trans_func = func_from_string(trans)
+                for key in m_dict.keys():
+                    para_lst = list(ParameterGrid(m_dict[key]))
+                    model_func = func_from_string(key)
+
+                    if trans_func.__name__ == 'BinningProcess':
+                        _models = {
+                            f"{trans_func.__name__}_{model_func.__name__}_{'_'.join([f'{x}{y}' for (x,y) in list(p.items())])}":
+                                {'model':Pipeline([('transformation',trans_func(get_feat(self.config))),('model',model_func(**p))])} 
+                            for p in para_lst  
+                            }
+                    else:                    
+                        _models = {
+                            f"{trans_func.__name__}_{model_func.__name__}_{'_'.join([f'{x}{y}' for (x,y) in list(p.items())])}":
+                                {'model':Pipeline([('transformation',trans_func()),('model',model_func(**p))])} 
+                            for p in para_lst  
+                            }
+                    models.update(_models)
+        
+        if self.config['use_binning'] in [True, 'Both']:
             for key in m_dict.keys():
                 para_lst = list(ParameterGrid(m_dict[key]))
                 model_func = func_from_string(key)
-
-                if trans_func.__name__ == 'BinningProcess':
-                    _models = {
-                        f"{trans_func.__name__}_{model_func.__name__}_{'_'.join([f'{x}{y}' for (x,y) in list(p.items())])}":
-                            {'model':Pipeline([('transformation',trans_func(get_feat(config))),('model',model_func(**p))])} 
-                        for p in para_lst  
-                        }
-                else:                    
-                    _models = {
-                        f"{trans_func.__name__}_{model_func.__name__}_{'_'.join([f'{x}{y}' for (x,y) in list(p.items())])}":
-                            {'model':Pipeline([('transformation',trans_func()),('model',model_func(**p))])} 
-                        for p in para_lst  
-                        }
+                _models = {
+                    f"woe_{model_func.__name__}_{'_'.join([f'{x}{y}' for (x,y) in list(p.items())])}":
+                        {'model':Pipeline([('model',model_func(**p))])} 
+                    for p in para_lst  
+                    }
                 models.update(_models)
-    
-    if config['use_binning'] in [True, 'Both']:
-        for key in m_dict.keys():
-            para_lst = list(ParameterGrid(m_dict[key]))
-            model_func = func_from_string(key)
-            _models = {
-                f"woe_{model_func.__name__}_{'_'.join([f'{x}{y}' for (x,y) in list(p.items())])}":
-                    {'model':Pipeline([('model',model_func(**p))])} 
-                for p in para_lst  
-                }
-            models.update(_models)
 
 
-    return models
+        return models
 
+    def split_datasets(
+        self,
+        begin_training = pd.Timestamp("2015-01-01 00:00"),
+        end_training = pd.Timestamp("2019-12-31 23:00"),
+        begin_valid = pd.Timestamp("2020-01-01 00:00"),
+        end_valid = pd.Timestamp("2020-03-31 23:00"),
+        begin_test = pd.Timestamp("2020-04-01 00:00"),
+        end_test = pd.Timestamp("2020-06-15 23:00")
+    ):
+        """
+            Creates and returns all necessary Datasets for normal modellung and DMatrix elements for XGB
+        """   
 
-def split_datasets(
-    config,
-    begin_training = pd.Timestamp("2015-01-01 00:00"),
-    end_training = pd.Timestamp("2019-12-31 23:00"),
-    begin_valid = pd.Timestamp("2020-01-01 00:00"),
-    end_valid = pd.Timestamp("2020-03-31 23:00"),
-    begin_test = pd.Timestamp("2020-04-01 00:00"),
-    end_test = pd.Timestamp("2020-06-15 23:00")
-):
-    """
-        Creates and returns all necessary Datasets for normal modellung and DMatrix elements for XGB
-    """   
-
-    data = get_data(config)
-    use_binning = config['use_binning']
-    bads = config['bads']
-    tgt = config['target']
-    
-    collst =list(data.columns)
-    feat = sorted(list(set(collst)-set(bads)-set(tgt)))
-
-    if use_binning:       
-        bfile = config['dpath']+config['dbins']
-        bins = joblib.load(bfile)
-
-        # Always ensure DF are ordered
-        binned_feat = list(bins.keys())
-        binned_feat.sort()
-        feat = [x+'_woe' for x in binned_feat]    
+        data = get_data(self.config)
+        use_binning = self.config['use_binning']
+        bads = self.config['bads']
+        tgt = self.config['target']
         
-        dataset = sc.woebin_ply(data.loc[begin_training:end_training,binned_feat+[tgt]].dropna(), bins)
-        valiset = sc.woebin_ply(data.loc[begin_valid:end_valid,binned_feat+[tgt]].dropna(), bins)
-        testset = sc.woebin_ply(data.loc[begin_test:end_test,binned_feat+[tgt]].dropna(), bins)
+        collst =list(data.columns)
+        feat = sorted(list(set(collst)-set(bads)-set(tgt)))
 
-    else:
-        dataset = data.loc[begin_training:end_training,feat+[tgt]].dropna()
-        valiset = data.loc[begin_valid:end_valid,feat+[tgt]].dropna()
-        testset = data.loc[begin_test:end_test,feat+[tgt]].dropna()
+        if use_binning:       
+            bfile = self.config['dpath']+self.config['dbins']
+            bins = joblib.load(bfile)
 
-    X_train = dataset.drop(columns=[tgt])[feat]
-    X_valid = valiset.drop(columns=[tgt])[feat]
-    X_test  = testset.drop(columns=[tgt])[feat]
-    y_train = dataset[tgt].values
-    y_valid = valiset[tgt].values
-    y_test  = testset[tgt].values
+            # Always ensure DF are ordered
+            binned_feat = list(bins.keys())
+            binned_feat.sort()
+            feat = [x+'_woe' for x in binned_feat]    
+            
+            dataset = sc.woebin_ply(data.loc[begin_training:end_training,binned_feat+[tgt]].dropna(), bins)
+            valiset = sc.woebin_ply(data.loc[begin_valid:end_valid,binned_feat+[tgt]].dropna(), bins)
+            testset = sc.woebin_ply(data.loc[begin_test:end_test,binned_feat+[tgt]].dropna(), bins)
 
-    logging.info(f"Training Dataset {dataset.index.min()} {dataset.index.max()}")
-    logging.info(f"Validation Dataset {valiset.index.min()} {valiset.index.max()}")
-    logging.info(f"Test Dataset {testset.index.min()} {testset.index.max()}")
-
-    return y_train,y_valid,y_test,X_train,X_valid,X_test,dataset,valiset,testset
-
-
-def get_feat(config):
-    data = get_data(config)
-    bads = config['bads']
-    tgt = config['target']
-    
-    collst =list(data.columns)
-    feat = sorted(list(set(collst)-set(bads)-set(tgt)))
-    return feat
-
-def get_data(config):
-    datafile = config['dpath']+config['dvarsel']
-    if os.path.isfile(datafile) & config['use_binning']:
-        logging.info(f"Preprocessed Datafile found {datafile}")
-    elif config['use_binning'] == False:
-        datafile = config['dpath']+config['data']
-        logging.info(f"Use Binning is False. Use {datafile}")    
-    else:
-        datafile = config['dpath']+config['data']
-        logging.info(f"No Preprocessed Datafile found. Default to {datafile}")
-
-    return pd.read_parquet(datafile)
-
-
-def mltrain_loop(
-    config = None,
-    refit_models=False,
-    use_binning=False,
-    **kwargs
-):
-
-    if config is None:
-        logging.info("No Config file provided")
-    else:
-        logging.info("Config file provided")
-
-        models = model_config(config)
-        data = get_data(config)
-        
-        cfg = config['begin_training']
-        logging.debug(f"Config begin_training: {cfg} of type {type(cfg)}")
-        if type(cfg) == list:
-            training_begins = [pd.Timestamp(x) for x in cfg]
         else:
-            offset_lst= config['offset_lst']
-            offset_res= config['offset_res']
-            training_begins = [pd.Timestamp(cfg)+pd.DateOffset(**{offset_res: offset}) for offset in offset_lst]
-        logging.debug(f"List of Training Begins: {training_begins}")
+            dataset = data.loc[begin_training:end_training,feat+[tgt]].dropna()
+            valiset = data.loc[begin_valid:end_valid,feat+[tgt]].dropna()
+            testset = data.loc[begin_test:end_test,feat+[tgt]].dropna()
 
-        end_training    = pd.Timestamp(config['end_training'])
-        begin_valid     = pd.Timestamp(config['begin_valid'])
-        end_valid       = pd.Timestamp(config['end_valid'])
-        begin_test      = pd.Timestamp(config['begin_test'])
-        end_test        = pd.Timestamp(config['end_test'])     
-        
-    mlist = list(models.keys())   
-    random.shuffle(mlist)
-    
-    logging.info(f"Fitting {len(mlist)} models for {len(training_begins)} training begin. Total: {len(mlist)*len(training_begins)}")
-    logging.debug(f"List of models {mlist}")
-    
-    cnt_begins = 1
-    for begin_training in training_begins:
+        X_train = dataset.drop(columns=[tgt])[feat]
+        X_valid = valiset.drop(columns=[tgt])[feat]
+        X_test  = testset.drop(columns=[tgt])[feat]
+        y_train = dataset[tgt].values
+        y_valid = valiset[tgt].values
+        y_test  = testset[tgt].values
 
-        y_train,y_valid,y_test,X_train,X_valid,X_test ,dataset,valiset,testset = \
-            split_datasets(
-                config,
-                begin_training = begin_training,
-                end_training = end_training,
-                begin_valid = begin_valid,
-                end_valid = end_valid,
-                begin_test = begin_test,
-                end_test = end_test
-                )       
-        
-        cnt_models=1
-        for m in mlist:
+        logging.info(f"Training Dataset {dataset.index.min()} {dataset.index.max()}")
+        logging.info(f"Validation Dataset {valiset.index.min()} {valiset.index.max()}")
+        logging.info(f"Test Dataset {testset.index.min()} {testset.index.max()}")
 
-            # Specify Output filenames
-            Path('models/').mkdir(parents=True, exist_ok=True)
-            model_fname = "models/{}_{}.dat".format(m,begin_training.strftime("%Y%m%d"))
-            Path('metrics/').mkdir(parents=True, exist_ok=True)
-            metric_fname = "metrics/{}_{}.csv".format(m,begin_training.strftime("%Y%m%d"))
+        return y_train,y_valid,y_test,X_train,X_valid,X_test,dataset,valiset,testset
 
-            logging.debug(f"Model {m}: Starting")
+    def mltrain_loop(
+        self,
+        refit_models=False,
+        use_binning=False,
+        **kwargs
+    ):
 
-            if os.path.isfile(model_fname):
-                if refit_models == False:
-                    logging.info(f"{cnt_begins}/{len(training_begins)} {cnt_models}/{len(mlist)} Completed Model {m} already exists. No refitting.")
-                    cnt_models = cnt_models+1
-                    if cnt_models == len(mlist):
-                        cnt_begins = cnt_begins + 1
-                    continue
-                logging.debug(f"Model {m}: Refit model")
+        if self.config is None:
+            logging.info("No self.config file provided")
+        else:
+            logging.info("self.config file provided")
+
+            models = self.model_config()
+            data = get_data(self.config)
             
-            logging.debug(f"Model {m}: Fitting started")
-
-            model = models[m]['model']
-
-            if 'fit_params' in models[m].keys():
-                fit_params = models[m]['fit_params']
-                fit_params['eval_set'] = [(X_valid,y_valid)]
-                model.fit(X_train,y_train,**fit_params)
+            cfg = self.config['begin_training']
+            logging.debug(f"Config begin_training: {cfg} of type {type(cfg)}")
+            if type(cfg) == list:
+                training_begins = [pd.Timestamp(x) for x in cfg]
             else:
-                model.fit(X_train,y_train)
+                offset_lst= self.config['offset_lst']
+                offset_res= self.config['offset_res']
+                training_begins = [pd.Timestamp(cfg)+pd.DateOffset(**{offset_res: offset}) for offset in offset_lst]
+            logging.debug(f"List of Training Begins: {training_begins}")
+
+            end_training    = pd.Timestamp(self.config['end_training'])
+            begin_valid     = pd.Timestamp(self.config['begin_valid'])
+            end_valid       = pd.Timestamp(self.config['end_valid'])
+            begin_test      = pd.Timestamp(self.config['begin_test'])
+            end_test        = pd.Timestamp(self.config['end_test'])     
             
-            joblib.dump(model, model_fname)
-            logging.debug(f"Model {m}: Fitting ended")
+        mlist = list(models.keys())   
+        random.shuffle(mlist)
+        
+        logging.info(f"Fitting {len(mlist)} models for {len(training_begins)} training begin. Total: {len(mlist)*len(training_begins)}")
+        logging.debug(f"List of models {mlist}")
+        
+        cnt_begins = 1
+        for begin_training in training_begins:
+
+            y_train,y_valid,y_test,X_train,X_valid,X_test ,dataset,valiset,testset = \
+                self.split_datasets(
+                    begin_training = begin_training,
+                    end_training = end_training,
+                    begin_valid = begin_valid,
+                    end_valid = end_valid,
+                    begin_test = begin_test,
+                    end_test = end_test
+                    )       
             
-            
-            results = pd.DataFrame(columns=pd.MultiIndex.from_product([[begin_training],['train', 'valid','test'], ['accuracy_score','roc_auc_score']]))
+            cnt_models=1
+            for m in mlist:
 
-            results.loc[m,(begin_training,'train','accuracy_score')]   = accuracy_score(y_train,model.predict(X_train))
-            results.loc[m,(begin_training,'train','roc_auc_score')]   = roc_auc_score(y_train,model.predict(X_train))
-            
-            results.loc[m,(begin_training,'valid','accuracy_score')]   = accuracy_score(y_valid,model.predict(X_valid))
-            results.loc[m,(begin_training,'valid','roc_auc_score')]   = roc_auc_score(y_valid,model.predict(X_valid))
+                # Specify Output filenames
+                Path('models/').mkdir(parents=True, exist_ok=True)
+                model_fname = "models/{}_{}.dat".format(m,begin_training.strftime("%Y%m%d"))
+                Path('metrics/').mkdir(parents=True, exist_ok=True)
+                metric_fname = "metrics/{}_{}.csv".format(m,begin_training.strftime("%Y%m%d"))
 
-            results.loc[m,(begin_training,'test','accuracy_score')]   = accuracy_score(y_test,model.predict(X_test))
-            results.loc[m,(begin_training,'test','roc_auc_score')]   = roc_auc_score(y_test,model.predict(X_test))
+                logging.debug(f"Model {m}: Starting")
 
-            results.to_csv(metric_fname)
-            logging.debug(f"Model {m}: Metrics Calculated")
+                if os.path.isfile(model_fname):
+                    if refit_models == False:
+                        logging.info(f"{cnt_begins}/{len(training_begins)} {cnt_models}/{len(mlist)} Completed Model {m} already exists. No refitting.")
+                        cnt_models = cnt_models+1
+                        if cnt_models == len(mlist):
+                            cnt_begins = cnt_begins + 1
+                        continue
+                    logging.debug(f"Model {m}: Refit model")
+                
+                logging.debug(f"Model {m}: Fitting started")
 
-            logging.info(f"{cnt_begins}/{len(training_begins)} {cnt_models}/{len(mlist)} Completed Model {m}")
-            cnt_models=cnt_models+1
-        cnt_begins = cnt_begins + 1
-    
+                model = models[m]['model']
 
-def extract_best_model(metric='accuracy_score',ds='valid'):
-    results=pd.DataFrame()
-    for file in os.listdir('metrics'):
-        #print(file)
-        _r = pd.read_csv(f"metrics/{file}",header=[0,1,2],index_col=[0],parse_dates=True)
-        results = pd.concat([results,_r.stack(level=0)],axis=0)
+                if 'fit_params' in models[m].keys():
+                    fit_params = models[m]['fit_params']
+                    fit_params['eval_set'] = [(X_valid,y_valid)]
+                    model.fit(X_train,y_train,**fit_params)
+                else:
+                    model.fit(X_train,y_train)
+                
+                joblib.dump(model, model_fname)
+                logging.debug(f"Model {m}: Fitting ended")
+                
+                
+                results = pd.DataFrame(columns=pd.MultiIndex.from_product([[begin_training],['train', 'valid','test'], ['accuracy_score','roc_auc_score']]))
 
-        results.index.set_names(['model', 'begin'],inplace=True)
+                results.loc[m,(begin_training,'train','accuracy_score')]   = accuracy_score(y_train,model.predict(X_train))
+                results.loc[m,(begin_training,'train','roc_auc_score')]   = roc_auc_score(y_train,model.predict(X_train))
+                
+                results.loc[m,(begin_training,'valid','accuracy_score')]   = accuracy_score(y_valid,model.predict(X_valid))
+                results.loc[m,(begin_training,'valid','roc_auc_score')]   = roc_auc_score(y_valid,model.predict(X_valid))
 
-        res = results[(ds,metric)].reset_index()
+                results.loc[m,(begin_training,'test','accuracy_score')]   = accuracy_score(y_test,model.predict(X_test))
+                results.loc[m,(begin_training,'test','roc_auc_score')]   = roc_auc_score(y_test,model.predict(X_test))
 
-    ilst = []
-    for group in res.groupby('model'):
-        #display(group[1])
-        imax = group[1][(ds,metric)].idxmax()
-        #print(imax)
-        ilst.append(imax)
+                results.to_csv(metric_fname)
+                logging.debug(f"Model {m}: Metrics Calculated")
 
-    bestmodel = res.loc[ilst]
-    bestmodel.columns = ['model','begin',metric]
-    bestmodel['model_fname'] = [f"models/{m}_{pd.Timestamp(b).strftime('%Y%m%d')}.dat" for m,b in zip(bestmodel.model,bestmodel.begin)]
-
-    return bestmodel.sort_values(by=metric,ascending=False)
+                logging.info(f"{cnt_begins}/{len(training_begins)} {cnt_models}/{len(mlist)} Completed Model {m}")
+                cnt_models=cnt_models+1
+            cnt_begins = cnt_begins + 1
